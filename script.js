4e9d1ce37d12f4d65aa59825d216fbcf577a45cf
Enzo Coronel <enzocoronel1000@gmail.com>
2022-12-02 13:28:36 -0300

    desktop_dropdown_added
    All 5 desktop dropdown added, need further adjustments (style)

--- script.js
+++ script.js
@@ -1,28 +1,106 @@
 const categoryDrop = () => {
-  document.getElementById("firstdrop").classList.toggle("show");
+  document.getElementById("firstdrop").classList.toggle("show-block");
+};
+
+const searchDrop = () => {
+  let x = document.getElementById("search-toggle");
+  x.classList.toggle("search-active");
+  document.getElementsByTagName("main")[0].classList.toggle("darken");
+  document.getElementsByTagName("footer")[0].classList.toggle("darken");
+  document.getElementsByTagName("body")[0].classList.toggle("scroll");
+  document.getElementById("top-correct").classList.toggle("hide");
 };
 
 const navDrop = () => {
-  let x = document.getElementById("nav-drop");
-    x.style.display = "block";
-    document.getElementsByTagName("main")[0].style.filter = "brightness(40%)";
-    document.getElementsByTagName("footer")[0].style.filter = "brightness(40%)";
-}
+  let x = document.getElementById("nav-toggle");
+  x.classList.add("nav-active");
+  document.getElementsByTagName("main")[0].classList.add("darken");
+  document.getElementsByTagName("footer")[0].classList.add("darken");
+  document.getElementsByTagName("body")[0].classList.add("scroll");
+};
+
+const storeDrop = () => {
+  let x = document.getElementById("store-toggle");
+  x.classList.toggle("store-active");
+  document.getElementsByTagName("main")[0].classList.toggle("darken");
+  document.getElementsByTagName("footer")[0].classList.toggle("darken");
+  document.getElementsByTagName("body")[0].classList.toggle("scroll");
+};
 
 const dropGroup = (group) => {
+  let child = document.getElementsByClassName("drop-group");
   let x = document.getElementById(group);
-    x.style.display = "flex";
+  if (x.classList.contains("show-flex")) {
+    document.getElementsByTagName("main")[0].classList.remove("darken");
+    document.getElementsByTagName("footer")[0].classList.remove("darken");
+    document.getElementById("nav-toggle").classList.remove("nav-active");
+    document.getElementsByTagName("body")[0].classList.remove("scroll");
+  } else {
+    for (i = 0; i < child.length; i++) {
+      child[i].classList.remove("show-flex");
+    }
+    x.classList.toggle("show-flex");
+  }
 };
 
 window.onclick = function (event) {
-  if (!event.target.matches(".link")) {
-    let dropdown = document.getElementById("nav-drop");
-    let child = document.getElementsByClassName("drop-group");
-    if (dropdown.style.display !== "none") {
-      for (i = 0; i < child.length; i++) {
-        child[i].style.display = "none";
-      }
-      dropdown.style.display = "none";
+  if (event.target == document.getElementByTagName("header")[0]) {
+    let dropdown = document.getElementById("nav-toggle");
+    if (dropdown.classList.contains("nav-active")) {
+      dropdown.classList.remove("nav-active");
+      document.getElementsByTagName("main")[0].classList.remove("darken");
+      document.getElementsByTagName("footer")[0].classList.remove("darken");
+      document.getElementsByTagName("body")[0].classList.remove("scroll");
     }
   }
 };
+
+//
+
+const openModal = () => {
+  document.getElementById("modal").style.display = "block";
+  document.getElementsByTagName("header")[0].style.filter =
+    "brightness(40%) blur(15px)";
+  document.getElementsByTagName("main")[0].style.filter =
+    "brightness(40%) blur(15px)";
+  document.getElementsByTagName("footer")[0].style.filter =
+    "brightness(40%) blur(15px)";
+};
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+  document.getElementsByTagName("header")[0].style.filter = "none";
+  document.getElementsByTagName("main")[0].style.filter = "none";
+  document.getElementsByTagName("footer")[0].style.filter = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
